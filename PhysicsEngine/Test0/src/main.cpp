989167cd5c6e972ea5e1f15828559464c3bc2dba
Wannes Degelin Willekens <degelin.wannes@gmail.com>
2026-04-17 16:12:41 +0200

    Started testing directx12 as a base for the engine

---
+++ PhysicsEngine/Test0/src/main.cpp
@@ -0,0 +1,248 @@
+﻿// code inspired by https://www.3dgep.com/learning-directx-12-1/#CMake
+#define WIND32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <shellapi.h>
+#include <wrl.h>
+using namespace Microsoft::WRL;
+
+// Windows min max macros can cause naming conflicts
+#if defined(min)
+#undef min
+#endif // defined(min)
+
+
+#if defined(max)
+#undef max
+#endif // defined(max)
+
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+
+#include <d3dx12.h>
+
+#include <algorithm>
+#include <cassert>
+#include <chrono>
+
+#include <Helpers.h>
+
+// amount of swap chain back buffers
+const uint8_t g_NumFrames{ 3 };
+
+bool g_UseWarp{ false };
+
+uint32_t g_ClientWidth{ 1280 };
+uint32_t g_ClientHeight{ 720 };
+
+bool g_IsInitialized{ false };
+
+// window handle
+HWND g_hwnd{};
+
+// window rect
+RECT g_WindowRect{};
+
+// Dx12 objects
+ComPtr<ID3D12Device2> g_Device{};
+ComPtr<ID3D12CommandQueue> g_CommandQueue{};
+ComPtr<IDXGISwapChain4> g_SwapChain{};
+ComPtr<ID3D12Resource> g_BackBuffers[g_NumFrames]{};
+ComPtr<ID3D12GraphicsCommandList> g_CommandList{};
+ComPtr<ID3D12CommandAllocator> g_CommandAlloctors[g_NumFrames]{};
+ComPtr<ID3D12DescriptorHeap> g_RTVDescriptorHeap{};
+UINT g_RTVDescriptorSize{};
+UINT g_CurrentBackBufferIndex{};
+
+// sync object
+ComPtr<ID3D12Fence> g_Fence{};
+uint64_t g_FenceValue{ 0 };
+uint64_t g_FrameFenceValues[g_NumFrames]{};
+HANDLE g_FenceEvent{};
+
+bool g_VSync{ true };
+bool g_TearingSupported{ false };
+bool g_Fullscreen{ false };
+
+// window callback function foward declaration
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+void ParseCommandLineArguments()
+{
+	int argc;
+	wchar_t** argv = ::CommandLineToArgvW(::GetCommandLineW(), &argc);
+
+	for (size_t i = 0; i < argc; i++)
+	{
+        if (std::wcscmp(argv[i], L"-w") == 0 || std::wcscmp(argv[i], L"--width") == 0)
+            g_ClientWidth = std::wcstol(argv[++i], nullptr, 10);
+
+        if (std::wcscmp(argv[i], L"-h") == 0 || std::wcscmp(argv[i], L"--height") == 0)
+            g_ClientHeight = std::wcstol(argv[++i], nullptr, 10);
+        
+        if (std::wcscmp(argv[i], L"-warp") == 0 || std::wcscmp(argv[i], L"--warp") == 0)
+            g_UseWarp = true;
+	}
+
+    ::LocalFree(argv);
+}
+
+void EnableDebugLayer()
+{
+#if defined(_DEBUG)
+    ComPtr<ID3D12Debug> debugInterface{};
+    ThrowIfFailed(D3D12GetDebugInterface(IID_PPV_ARGS(&debugInterface)));
+    debugInterface->EnableDebugLayer();
+#endif // defined(_DEBUG)s
+}
+
+void RegisterWindowClass(HINSTANCE hInst, const wchar_t* windowClassName)
+{
+    WNDCLASSEXW windowClass{};
+
+    windowClass.cbSize = sizeof(WNDCLASSEX);
+    windowClass.style = CS_HREDRAW | CS_VREDRAW;
+    windowClass.lpfnWndProc = &WndProc;
+    windowClass.cbClsExtra = 0;
+    windowClass.cbWndExtra = 0;
+    windowClass.hInstance = hInst;
+    windowClass.hIcon = ::LoadIcon(hInst, NULL);
+    windowClass.hCursor = ::LoadCursor(NULL, IDC_ARROW);
+    windowClass.hbrBackground = reinterpret_cast<HBRUSH>(COLOR_WINDOW + 1);
+    windowClass.lpszMenuName = NULL;
+    windowClass.lpszClassName = windowClassName;
+    windowClass.hIconSm = ::LoadIcon(hInst, NULL);
+
+    static ATOM atom{ ::RegisterClassExW(&windowClass) };
+    assert(atom > 0);
+}
+
+HWND CreateDXWindow(const wchar_t* windowClassName, HINSTANCE hInst, const wchar_t* windowTitle,
+    uint32_t width, uint32_t height)
+{
+    int screenWidth{ ::GetSystemMetrics(SM_CXSCREEN) };
+    int screenHeight{ ::GetSystemMetrics(SM_CYSCREEN) };
+
+    RECT windowRect{ 0,0,static_cast<LONG>(width), static_cast<LONG>(height) };
+    ::AdjustWindowRect(&windowRect, WS_OVERLAPPEDWINDOW, FALSE);
+
+    int windowWidth = windowRect.right - windowRect.left;
+    int windowHeight = windowRect.bottom - windowRect.top;
+
+    int windowX = std::max<int>(0, (screenWidth - windowWidth) * .5);
+    int windowY = std::max<int>(0, (screenHeight - windowHeight) * .5);
+
+    HWND hWnd = ::CreateWindowExW(
+        NULL,
+        windowClassName,
+        windowTitle,
+        WS_OVERLAPPEDWINDOW,
+        windowX,
+        windowY,
+        windowWidth,
+        windowHeight,
+        NULL,
+        NULL,
+        hInst,
+        nullptr
+    );
+
+    assert(hWnd && "Failed to create window");
+
+    return hWnd;
+}
+
+ComPtr<IDXGIAdapter4> GetAdapter(bool useWarp)
+{
+    ComPtr<IDXGIFactory4> dxgiFactory{};
+    UINT createFactoryFlags = 0;
+
+#if defined(_DEBUG)
+    createFactoryFlags = DXGI_CREATE_FACTORY_DEBUG;
+#endif // defined(_DEBUG)
+
+    ThrowIfFailed(CreateDXGIFactory2(createFactoryFlags, IID_PPV_ARGS(&dxgiFactory)));
+
+    ComPtr<IDXGIAdapter1> dxgiAdapter1{};
+    ComPtr<IDXGIAdapter4> dxgiAdapter4{};
+
+    if (useWarp)
+    {
+        ThrowIfFailed(dxgiFactory->EnumWarpAdapter(IID_PPV_ARGS(&dxgiAdapter1)));
+        ThrowIfFailed(dxgiAdapter1.As(&dxgiAdapter4));
+        return NULL;
+    }
+
+    SIZE_T maxDedicatedVideoMemory{ 0 };
+    for (UINT i = 0; dxgiFactory->EnumAdapters1(i,&dxgiAdapter1) != DXGI_ERROR_NOT_FOUND; i++)
+    {
+        DXGI_ADAPTER_DESC1 dxgiAdapterDesc1{};
+        dxgiAdapter1->GetDesc1(&dxgiAdapterDesc1);
+
+        if ((dxgiAdapterDesc1.Flags & DXGI_ADAPTER_FLAG_SOFTWARE) == 0 &&
+            SUCCEEDED(D3D12CreateDevice(dxgiAdapter1.Get(), D3D_FEATURE_LEVEL_11_0, __uuidof(ID3D12Device), nullptr)) &&
+            dxgiAdapterDesc1.DedicatedVideoMemory > maxDedicatedVideoMemory)
+        {
+            maxDedicatedVideoMemory = dxgiAdapterDesc1.DedicatedVideoMemory;
+            ThrowIfFailed(dxgiAdapter1.As(&dxgiAdapter4));
+        }
+    }
+
+    return dxgiAdapter4;
+}
+
+ComPtr<ID3D12Device2> CreateDevice(ComPtr<IDXGIAdapter4> adapter)
+{
+    ComPtr<ID3D12Device2> d3d12Device2{};
+    ThrowIfFailed(D3D12CreateDevice(adapter.Get(), D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&d3d12Device2)));
+
+    // debug messages for debug mode
+#if defined(_DEBUG)
+    ComPtr<ID3D12InfoQueue> pInfoQueue{};
+    if (SUCCEEDED(d3d12Device2.As(&pInfoQueue)))
+    {
+        pInfoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_CORRUPTION, TRUE);
+        pInfoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_ERROR, TRUE);
+        pInfoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_WARNING, TRUE);
+
+        // Suppress messages based on their severity level
+        D3D12_MESSAGE_SEVERITY Severities[] =
+        {
+            D3D12_MESSAGE_SEVERITY_INFO
+        };
+
+        // Suppress individual messages by their ID
+        D3D12_MESSAGE_ID DenyIds[] = {
+            D3D12_MESSAGE_ID_CLEARRENDERTARGETVIEW_MISMATCHINGCLEARVALUE,   // not sure how to avoid this message.
+            D3D12_MESSAGE_ID_MAP_INVALID_NULLRANGE,                         // This warning occurs when using capture frame while graphics debugging.
+            D3D12_MESSAGE_ID_UNMAP_INVALID_NULLRANGE,                       // This warning occurs when using capture frame while graphics debugging.
+        };
+
+        D3D12_INFO_QUEUE_FILTER NewFilter = {};
+        NewFilter.DenyList.NumSeverities = _countof(Severities);
+        NewFilter.DenyList.pSeverityList = Severities;
+        NewFilter.DenyList.NumIDs = _countof(DenyIds);
+        NewFilter.DenyList.pIDList = DenyIds;
+
+        ThrowIfFailed(pInfoQueue->PushStorageFilter(&NewFilter));
+    }
+#endif // defined(_DEBUG)
+
+    return d3d12Device2;
+}
+
+ComPtr<ID3D12CommandQueue> CreateCommandQueue(ComPtr<ID3D12Device2> device, D3D12_COMMAND_LIST_TYPE type)
+{
+    ComPtr<ID3D12CommandQueue> d3d12CommandQueue;
+
+    D3D12_COMMAND_QUEUE_DESC desc{};
+    desc.Type = type;
+    desc.Priority = D3D12_COMMAND_QUEUE_PRIORITY_NORMAL;
+    desc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+    desc.NodeMask = 0;
+
+    ThrowIfFailed(device->CreateCommandQueue(&desc, IID_PPV_ARGS(&d3d12CommandQueue)));
+
+    return d3d12CommandQueue;
+}
